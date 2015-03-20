bdda89bd21aeb990ade4528c207b94a69d9740d4
AutoCoder <tj_liyuan@163.com>
2015-03-20 09:16:38 +0800

    add ffmpeg python wrapper

--- VToy/Public/Utils.py
+++ VToy/Public/Utils.py
@@ -19,3 +19,27 @@ def genQRImage(macAddress, qrTicket, path=''):
 	filepath += '.png'
 	cmd = 'qrencode -o %s -v 5 -l Q "%s"' % (filepath, qrTicket)
 	return os.system(cmd), filepath
+
+def wav2amr(wavfile, amrfile):
+	"""
+	convert wav to amr by ffmpeg
+	1rd parameter is inpute file
+	2rd parameter is output file
+	"""
+	if os.path.exists(wavfile):
+		cmd = "ffmpeg -i %s -ab 12.2k -ar 8000 -ac 1 %s" % (wavfile, amrfile)
+		return (os.system(cmd) == 0), amrfile
+	else:
+		return False, "wav file is not existed, please check the file path"
+
+def amr2wav(amrfile, wavfile):
+	"""
+	convert amr to wav by ffmpeg
+	1rd parameter is inpute file
+	2rd parameter is output file
+	"""
+	if os.path.exists(amrfile):
+		cmd = "ffmpeg -i %s %s" % (amrfile, wavfile)
+		return (os.system(cmd) == 0), wavfile
+	else:
+		return False, "amr file is not existed, please check the file path"
